b14fbb1029a63126962ee5be4331a7accc763467
Timothy Qian <12tqian@gmail.com>
2025-02-11 15:10:04 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static export
-  basePath: "/12tqian.github.io", // Change this to your repo name
-  assetPrefix: "/12tqian.github.io/", // Ensures assets are loaded correctly
   images: {
     unoptimized: true, // GitHub Pages does not support Next.js image optimization
   },
-  trailingSlash: true, // Ensures that the site works with GitHub Pages
   distDir: "build", // Changes the build directory to "build"
 };
 
