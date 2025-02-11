4180c9cd39c63168df52f5d352a4107d230b2dbc
Timothy Qian <12tqian@gmail.com>
2025-02-10 23:06:33 -0500

    pfp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="w-full flex flex-col items-center">
@@ -5,15 +7,25 @@ export default function Home() {
         <div className="flex flex-col w-full items-start">
           <div>
             <p>
-              I am an MEng student at MIT studying Electrical Engineering and
+              I'm an MEng student at MIT studying Electrical Engineering and
               Computer Science.
             </p>
+
+            <p>I like language models and over-optimizing computer programs.</p>
+
             <p>
-              Please contact me at tcqian [at] mit [dot] edu if you would like
+              Please talk to me at tcqian [at] mit [dot] edu if you would like
               to chat!
             </p>
           </div>
         </div>
+        <Image
+          src="/images/me.jpg"
+          alt="Picture of the author"
+          className="mt-8"
+          width={400}
+          height={400}
+        />
       </div>
     </main>
   );
