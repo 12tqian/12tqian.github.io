34e9fcee5f895a5877901247f13025ac215b1292
Timothy Qian <12tqian@gmail.com>
2025-02-11 20:46:02 -0500

    fix weird entrance

--- src/app/components/PageWrapper.tsx
+++ src/app/components/PageWrapper.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import { motion, AnimatePresence } from "framer-motion";
-
 import { usePathname } from "next/navigation";
-
 import { ReactNode } from "react";
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        key={usePathname()}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1.1 }} // Adjust fade duration as needed
+        key={pathname}
+        initial={{ opacity: 0, y: 10 }} // Fade in + slide
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -10, transition: { duration: 0 } }} // Instant exit
+        transition={{ duration: 0.5, ease: "easeOut" }} // Smooth entry
         className="w-full"
       >
         {children}
